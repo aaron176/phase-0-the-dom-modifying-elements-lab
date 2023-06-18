6777c0504f6e3c47c19f4ff7a950d62f67906f75
haroon <haroon.seena2017@gmail.com>
2023-06-18 17:06:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+var div = document.createElement('div');
+var p = document.createElement('p');
+p.innerHTML = 'hello there';
+div.classList.add("big guy");
+div.classList.remove("guy")
+
